98dee805ce230e7f291372c6dffd87930c74cad5
declonetrooper <bjorncdittrich@gmail.com>
2022-10-12 08:34:01 -0400

    Last of Week 3 backend changes

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -11,7 +11,7 @@ class CarModelInline(admin.StackedInline):
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ['make', 'name', 'dealerid', 'types', 'year']
-    list_filter = ['types', 'make', 'dealerid', 'year',]
+    list_filter = ['cartype', 'make', 'dealerid', 'year',]
     search_fields = ['make', 'name']
 
 # CarMakeAdmin class with CarModelInline
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
+from .models import *
 # from .restapis import related methods
+from .restapis import *
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -85,16 +87,66 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = "https://us-south.functions.appdomain.cloud/api/v1/web/testinghelp_djangoserver-space/dealership-package/get-dealership.json"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    context = {}
+    if request.method == "GET":
+        url = 'https://us-south.functions.appdomain.cloud/api/v1/web/testinghelp_djangoserver-space/dealership-package/get-reviews.json'
+        reviews = get_dealer_reviews_from_cf(url, dealer_id)
+        # Concat all dealer's short name
+        reviews_text = ' '.join(["Review: " + review.review + " sentiment: " + review.sentiment + "\n" for review in reviews])
+        # Return a list of dealer short name
+        return HttpResponse(reviews_text)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
-# ...
+def add_review(request, dealer_id):
+    context = {}
+    dealer_url = 'https://us-south.functions.appdomain.cloud/api/v1/web/testinghelp_djangoserver-space/dealership-package/get-dealership-state.json'
+    dealer = get_dealer_by_id_from_cf(dealer_url, id=dealer_id)
+    context["dealer"] = dealer
+    if request.method == 'GET':
+        # Get cars for the dealer
+        cars = CarModel.objects.filter(id=dealer_id)
+        print(cars)
+        context["cars"] = cars
+        
+        return render(request, 'djangoapp/add_review.html', context)
+    elif request.method == 'POST':
+        if request.user.is_authenticated:
+            username = request.user.username
+            print(request.POST)
+            payload = dict()
+            car_id = request.POST["car"]
+            car = CarModel.objects.get(pk=car_id)
+            payload["time"] = datetime.utcnow().isoformat()
+            payload["name"] = username
+            payload["dealership"] = dealer_id
+            payload["id"] = dealer_id
+            payload["review"] = request.POST["content"]
+            payload["purchase"] = False
+            if "purchasecheck" in request.POST:
+                if request.POST["purchasecheck"] == 'on':
+                    payload["purchase"] = True
+            payload["purchase_date"] = request.POST["purchasedate"]
+            payload["car_make"] = car.make.name
+            payload["car_model"] = car.name
+            payload["car_year"] = int(car.year.strftime("%Y"))
+
+            new_payload = {}
+            new_payload["review"] = payload
+            review_post_url = "https://us-south.functions.appdomain.cloud/api/v1/web/testinghelp_djangoserver-space/dealership-package/post-review.json"
+            post_request(review_post_url, new_payload, id=dealer_id)
+        return redirect("djangoapp:dealer_details", id=dealer_id)
 
